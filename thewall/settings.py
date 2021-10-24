aa029c8daeb38479d428ce328e781fc1de403467
minh832000 <18521111@gm.uit.edu.vn>
2021-10-25 01:09:20 +0700

    add models

--- thewall/settings.py
+++ thewall/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'profiles.apps.ProfilesConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
