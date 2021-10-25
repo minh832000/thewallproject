7890c8d043d82b81a01e7a148e411aea7306490b
minh832000 <18521111@gm.uit.edu.vn>
2021-10-25 20:25:00 +0700

    add nee app accounts

---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser 
+
+# Create your models here.
+class User(AbstractUser):
+      is_job_seeker = models.BooleanField(default=False)
+      is_recruiter = models.BooleanField(default=False)
--- profiles/forms.py
+++ profiles/forms.py
@@ -1,15 +1,15 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.db import transaction
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from django.db import transaction
 
 
-class JobSeekerSignUpForm(UserCreationForm):
-      class Meta(UserCreationForm.Meta):
-            model = UserCreationForm
+# class JobSeekerSignUpForm(UserCreationForm):
+#       class Meta(UserCreationForm.Meta):
+#             model = UserCreationForm
       
-      @transaction.atomic
-      def save(self):
-            user = super().save(commit=False)
-            user.is_job_seeker = True
-            user.save()
-            return user
+#       @transaction.atomic
+#       def save(self):
+#             user = super().save(commit=False)
+#             user.is_job_seeker = True
+#             user.save()
+#             return user
