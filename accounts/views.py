5fcb75581822b2016531b593d8a0b7c93833ad1c
thaonhu24 <thaonhu1240@gmail.com>
2021-10-27 01:35:42 +0700

    add register_success page

--- accounts/views.py
+++ accounts/views.py
@@ -9,3 +9,6 @@ class jobseeker_register(CreateView):
       model = User
       form_class = JobSeekerSignUpForm
       template_name = 'accounts/jobseeker_register.html'
+
+def successView(request):
+      return render(request, 'accounts/register_success.html')
