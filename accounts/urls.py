99ce7acea02c05969c88d9ef4eebf6afc2842be6
thaonhu24 <thaonhu1240@gmail.com>
2021-10-28 02:07:54 +0700

    .......

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-      path('jobseeker_register/', views.jobseeker_register.as_view(), name='jobseeker_register'),
-      path('jobseeker_register/ok/', views.register_ok, name='register_ok'),
+      path('jobseeker/', views.jobseeker_register.as_view(), name='jobseeker_register'),
+      path('jobseeker/ok/', views.register_ok, name='register_ok'),
 ]
--- thewall/urls.py
+++ thewall/urls.py
@@ -21,6 +21,7 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('', include('home.urls')),
+    path('register/', include('accounts.urls')),
     path('posts/', include('posts.urls')),
 ]
