175a5bcb345aba6c3c0e14d36531f61cd3b3d507
thaonhu24 <thaonhu1240@gmail.com>
2021-10-29 01:25:26 +0700

    ........

--- accounts/views.py
+++ accounts/views.py
@@ -3,19 +3,21 @@ from django.shortcuts import redirect, render
 from django.urls.base import reverse_lazy
 from django.views.generic import CreateView
 from django.urls import reverse
+from django.views.generic.edit import FormView
 
-from .models import User, JobSeeker, Recruiter
-from .forms import JobSeekerSignUpForm, RecruiterSignUpForm
+from .models import User
+from .forms import JobSeekerSignUpForm
 
 # Create your views here.
 def register_ok(request):
       return render(request, 'accounts/register_success.html')
+      # template_name = 'accounts/register_success.html'
       
-class jobseeker_register(CreateView):
+class jobseeker_register(FormView):
       model = User
       form_class = JobSeekerSignUpForm
       template_name = 'accounts/jobseeker_register.html'
 
       def form_valid(self, form):
-            url = reverse_lazy('register_ok')
-            return redirect(reverse(url))
+            # url = reverse_lazy()
+            return redirect(reverse('register_ok'))
