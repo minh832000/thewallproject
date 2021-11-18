975f6e78b146804ab3a81bac947ff0912b475214
thaonhu24 <thaonhu1240@gmail.com>
2021-11-19 01:07:02 +0700

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
       path('register/', views.Register.as_view(), name='register'),
       path('login/', views.Login.as_view(), name='login'),
+      path('logout/', views.logout_view, name='logout'),
       path('register-success', views.view_register_success, name='register_success'),
       path('recruiter/register/', views.RecruiterRegister.as_view(), name='recruiter_register'),
 ]
