6efbb5bc7f7ecd4a8b4468f79cc721dad23acfce
minh832000 <18521111@gm.uit.edu.vn>
2021-11-21 15:34:50 +0700

    re-upload initial recruiter profile model

--- profiles/migrations/0002_initial_recruiterprofile.py
+++ profiles/migrations/0002_initial_recruiterprofile.py
@@ -6,10 +6,11 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
+    
+    initial = True 
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('profiles', '0005_profile_is_updated_summary'),
     ]
 
     operations = [
@@ -17,7 +18,9 @@ class Migration(migrations.Migration):
             name='RecruiterProfile',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('is_updated_basic_information_company', models.BooleanField(default=False)),
                 ('name_of_company', models.CharField(blank=True, max_length=128, null=True)),
+                ('profile_picture_company', models.ImageField(blank=True, default='default-profile-picture.jpg', upload_to='profile-picture')),
                 ('location_of_company', models.TextField(blank=True, max_length=256, null=True)),
                 ('email_of_company', models.EmailField(blank=True, max_length=254, null=True)),
                 ('hotline_of_company', models.TextField(blank=True, max_length=20, null=True)),
@@ -25,5 +28,9 @@ class Migration(migrations.Migration):
                 ('business_field_of_company', models.TextField(blank=True, max_length=128, null=True)),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profile_recruiter', to=settings.AUTH_USER_MODEL)),
             ],
+            options={
+                'verbose_name': 'Recruiter\'sProfile',
+                'verbose_name_plural': 'Recruiter\'s Profiles',
+            }
         ),
     ]
