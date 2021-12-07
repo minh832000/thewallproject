5463cdd9e9f069beeaf7c4f186a5d302106cb026
thaonhu24 <thaonhu1240@gmail.com>
2021-12-07 20:52:10 +0700

    commit

--- manage_recruiter/views.py
+++ manage_recruiter/views.py
@@ -59,20 +59,6 @@ def manageApplicant(request):
         "disable":" "
         })
 
-<<<<<<< HEAD
-@login_required
-def showApplicant(request):
-    if request.method=='POST':
-        id_post=request.POST.get('id-post')
-        post=Post_apply.objects.filter(post_apply_id=id_post)
-        list_applicant=[]
-        for applicant in post:
-            user=User.objects.get(id=applicant.user_apply_id)
-            item= Profile.objects.get(user_id=user.id)
-            list_applicant.append(item)
-            print(list_applicant)
-        return JsonResponse({'data':'success'})
-=======
 def accept(request):
     if request.method=="POST":
         idApply=request.POST.get("idApply")
@@ -177,4 +163,3 @@ def listRefuse(request):
         'list_candidate':list_cant,
         "disable":"disabled"
         })                  
->>>>>>> d922aa14b032cd125cc9bb4c73a1b200058b9778
